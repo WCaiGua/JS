62a9aefcf79bb413b9b1bdb464834ced2d535515
Bluelingling <2410546510@qq.com>
2019-02-18 17:04:28 +0800

    用构造函数实现队列

---
+++ Quene.js
@@ -0,0 +1,26 @@
+/**
+ * 构造函数  prototype是函数的属性，本质上是函数的原型对象
+ */
+var Quene = function(){
+    this.quene = [];
+};
+
+Quene.prototype.push = function(x){
+    this.quene[this.quene.length] = x;
+};
+Quene.prototype.pop = function(x){
+    this.quene.shift(x);
+}
+Quene.prototype.peek = function(){
+    if(this.quene.length>0){
+        return this.quene[0];
+    }
+};
+Quene.prototype.empty = function(){
+    return this.quene.length === 0 ? true : false;
+}
+//用队列新建一个实例才可以用到prototype属性  
+var quene1 =  new Quene();
+quene1.push(1);    //1进队列
+quene1.quene;      //当前数组
+
